640018eb0fc32f8846051d887087c99c28c5ff32
GaoZhiyuan <alapha23@gmail.com>
2018-07-03 19:24:27 +0000

    linear actuator

--- button.py
+++ button.py
@@ -6,9 +6,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN, GPIO.IN)
 
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+try:
+    while True:
+            input_state = GPIO.input(PIN)
+            if input_state == False:
+                print('Button Pressed')
+                time.sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
-while True:
-        input_state = GPIO.input(PIN)
-        if input_state == False:
-            print('Button Pressed')
-            time.sleep(0.2)
--- linear_actuator.py
+++ linear_actuator.py
@@ -1,3 +1,13 @@
+import RPi.GPIO as GPIO       
+import time                            
+
+PIN = 40
+
+GPIO.setmode(GPIO.BCM)       
+GPIO.setup(PIN, GPIO.OUT)             
+GPIO.output(PIN, 1)                      
+time.sleep(1)
+GPIO.cleanup()                         
 
 
 
