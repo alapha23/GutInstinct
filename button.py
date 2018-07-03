e4f6d4987f3a2266bf7969ce30f9558aea7d3514
GaoZhiyuan <alapha23@gmail.com>
2018-07-03 19:13:44 +0000

    added code for button

---
+++ button.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+PIN = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN, GPIO.IN)
+
+GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+        input_state = GPIO.input(PIN)
+        if input_state == False:
+            print('Button Pressed')
+            time.sleep(0.2)
